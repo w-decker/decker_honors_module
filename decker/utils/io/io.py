2e90158783b29ec77dff0864fd7c4176512026cf
Will Decker <deckerwill7@gmail.com>
2024-03-20 10:04:31 -0500

    clean up and fix mergre conflicts

--- decker/utils/io/io.py
+++ decker/utils/io/io.py
@@ -1,8 +1,100 @@
 from bids import BIDSLayout
 import bids.layout.layout
-import os
 from pathlib import Path
 from dataclasses import dataclass
+import pandas as pd
+import urllib.request
+import shutil
+import numpy as np
+import deepdish as dd
+from pathlib import Path
+
+def download_sherlock(out_dir: Path):
+    """Download sherlock dataset"""
+    out_dir.mkdir(exist_ok=True)
+
+    # sherlock data URLs
+    url_sherlock = 'https://ndownloader.figshare.com/files/9017983'
+    url_AG_movie = 'https://ndownloader.figshare.com/files/9055612'
+
+    # download data
+    with urllib.request.urlopen(url_sherlock) as response, open(out_dir / 'sherlock.h5', 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+
+    with urllib.request.urlopen(url_AG_movie) as response, open(out_dir / 'AG_movie_1recall.h5', 'wb') as out_file:
+        shutil.copyfileobj(response, out_file)
+
+def load_sherlock(path: Path, dataset: str) -> np.ndarray:
+    """Load sherlock data
+    
+    Parameters
+    ----------
+    path: str or os.path.object
+        Path to data directory
+
+    dataset: str
+        'sherlock' or 'AG_movie_1_recall'
+
+    Return
+    -------
+    D: numpy.ndarray
+    """
+
+    D = dd.io.load(path / f"{dataset}.h5")
+
+    print(f'Access variables like \n D["BOLD"] \n D["coords"] \n D["human_bounds"]')
+
+    return D
+
+def parse_pdb(path:str, subid_col: str, cond_col: str) -> dict:
+    """Read participant database and parse by condition
+    
+    Parameters
+    ----------
+    path: str
+        Path to wide formatted database
+
+    subid_col: str
+        Column name for subject identifiers
+
+    cond_col: str
+        Column name for condition identifiers
+        
+    Returns
+    -------
+    grouped: dict
+        Dictionary containg key-value pairs in which keys are condition factors and values are subject IDs"""
+
+    # get participant database
+    if path.endswith(".xlsx"):
+        df = pd.read_excel(path, header=0)
+    elif path.endswith(".csv"):
+        df = pd.read_csv(path)
+
+    # pivot with only subid and condition column
+    _df = df.pivot(columns=cond_col, values=subid_col)
+
+    # get conditions and create new dataframe
+    _, c = pd.factorize(df[cond_col])
+
+    # create empty dictionary
+    grouped = {}
+    for i in c.to_list():
+        grouped[i] = list(_df[i].dropna().unique())
+    
+    return grouped
+
+def get_subid(path: str) -> str:
+    """Extract the subject ID from a full BIDS file path to a func file in derivatives folder
+    
+    Parameters
+    ----------
+    path: str
+        Full path to file following bids naming and organization conventions"""
+
+    subid = path.split("/")[-3].split("-")[1]
+
+    return subid
 
 @dataclass
 class BIDSio(object):
